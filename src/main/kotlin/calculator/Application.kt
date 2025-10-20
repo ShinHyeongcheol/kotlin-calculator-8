2e5401621ae3eaadc625c7be51edc0fadf0c5469
SHC <hcshin0717@naver.com>
2025-10-20 21:00:04 +0900

    feat: extract a number from a input and output a sum of numbers

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,11 +2,24 @@ package calculator
 import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    // TODO: 프로그램 구현
     val input = readInput()
+    val numbers = extractNumbers(input)
+    val sum = numbers.sum()
+    println("결과 : $sum")
 }
 
 fun readInput(): String {
     println("덧셈할 문자열을 입력해 주세요.")
     return readLine()
+}
+
+fun extractNumbers(input: String): List<Int> {
+    if(input.isBlank()) return emptyList()
+    
+    val tokens = input.split(Regex("[,:]")).map { it.trim() }
+    
+    return tokens.mapNotNull { token ->
+        val digits = token.filter (Char::isDigit)
+        digits.takeIf {it.isNotEmpty()}?.toInt()
+    }
 }
